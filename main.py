8d039566bec5b2645ab1abdd73c6087a1ae2078c
zfima <zzfima@gmail.com>
2021-01-13 08:51:10 +0200

    explicitly to DataFrame

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataSet = pd.read_excel('trips_data.xlsx', index_col=0)
+dataSet = pd.DataFrame(pd.read_excel('trips_data.xlsx', index_col=0))
 print(dataSet.head(), '\n')
 print(dataSet.columns, '\n')
 print(dataSet.info(), '\n')
 
 print('All desc:\n', dataSet.describe(), '\n')
-print('salary desc:\n', dataSet.salary.describe(), '\n')
-dataSet.salary.hist()
+print('salary desc:\n', dataSet['salary'].describe(), '\n')
+dataSet['salary'].hist()
 plt.show()
 
-dataSet.age.hist()
+print('age desc:\n', dataSet['age'].describe(), '\n')
+dataSet['age'].hist()
+plt.show()
+
+print('city desc:\n', dataSet['city'].describe(), '\n')
+print(dataSet['city'].value_counts(), '\n')
+dataSet.city.hist(xrot=90)
 plt.show()
