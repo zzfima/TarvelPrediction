4c0f0e9bb292a4f0d95a330ed052628168b6b265
zfima <zzfima@gmail.com>
2021-01-13 08:35:09 +0200

    hist

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 dataSet = pd.read_excel('trips_data.xlsx', index_col=0)
-print(dataSet.head())
-print(dataSet.columns)
-print(dataSet.describe())
-print(dataSet.info())
+print(dataSet.head(), '\n')
+print(dataSet.columns, '\n')
+print(dataSet.info(), '\n')
+
+print('All desc:\n', dataSet.describe(), '\n')
+print('salary desc:\n', dataSet.salary.describe(), '\n')
+dataSet.salary.hist()
+plt.show()
